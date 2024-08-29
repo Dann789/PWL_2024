afa9b1a63143c3e43d46794d12d8add308f01362
Muhammad Wildan Ramadhana <wildan.rama2004@gmail.com>
2024-08-29 16:47:47 +0700

    add biodata

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Biodata Wildan</title>
+    <style>
+        h1 {
+            text-align: center;
+        }
+        table {
+            border: 2px solid black;
+            border-radius: 30px;
+            padding: 20px;
+            margin: 0 auto;
+            background-color:aqua;
+        }
+    </style>
+</head>
+<body>
+    <h1>Biodata Mahasiswa</h1>
+    <table>
+        <tr>
+            <td></td>
+            <td style="text-align: center; font-weight: 700">PROVINSI JAWA TIMUR</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td style="text-align: center; padding-bottom: 10px; font-weight: 700">KOTA MALANG</td>
+        </tr>
+        <tr>
+            <td style="font-weight: 700">NIK</td>
+            <td style="font-weight: 700">: 3573010........</td>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td>: Muhammad Wildan Ramadhana</td>
+        </tr>
+        <tr>
+            <td>Tempat/Tgl Lahir</td>
+            <td>: Malang, 02 November 2004</td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td>: Laki-laki</td>
+        </tr>
+        <tr>
+            <td>Alamat</td>
+            <td>: JL LA Sucipto XXIII/4 Blimbing</td>
+        </tr>
+        <tr>
+            <td>Agama</td>
+            <td>: Islam</td>
+        </tr>
+        <tr>
+            <td>Status Perkawinan</td>
+            <td>: Belum Kawin</td>
+            <td>KOTA MALANG</td>
+        </tr>
+        <tr>
+            <td>Pekerjaan</td>
+            <td>: Pelajar/Mahasiswa</td>
+            <td style="text-align: center">dd/mm/yyyy</td>
+        </tr>
+        <tr>
+            <td>Kewarganegaraan</td>
+            <td>: WNI</td>
+        </tr>
+    </table>
+</body>
+</html>
